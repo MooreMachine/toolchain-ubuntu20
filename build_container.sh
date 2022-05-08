436edf5c44d9e6604f3dd887e0e4c22fb55b4417
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2022-05-08 21:36:24 +0200

    001_automatic_version_number: Prompt to change version number.

--- build_container.sh
+++ build_container.sh
@@ -5,6 +5,25 @@ OLD_VERSION=$VERSION
 
 CHANGED_DOCKER=$(git diff --name-only Dockerfile)
 
+# If the Dockerfile has changed
+if [ ! -z ${CHANGED_DOCKER} ]
+then
+    printf 'Please consider updating the version number.\nCurrent version: %s\n' "$VERSION"
+    read -p "New version: " NEW_VERSION
+    if [ ! -z ${NEW_VERSION} ]
+    then
+        if [[ ! $NEW_VERSION =~ [0-9]+\.[0-9]+\.[0-9] ]]
+        then
+            printf 'Please provide a version number in the form of MAJOR.MINOR.PATCH\n'
+            exit 1
+        fi
+    else
+        printf 'Please provide a version number'
+        exit 1
+    fi
+    VERSION=$NEW_VERSION
+fi
+
 
 docker build --tag mooremachine/ubuntu20:$VERSION .
 
@@ -13,3 +32,6 @@ then
     echo "docker build failed!"
     exit 1
 fi
+
+printf 'Updating the version number in the README.md file...\n'
+sed -i '' 's/Version: '"$OLD_VERSION"'/Version: '"$VERSION"'/g' README.md
